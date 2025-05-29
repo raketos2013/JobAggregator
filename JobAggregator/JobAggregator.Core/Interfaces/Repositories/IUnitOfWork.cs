2a6e292d43bd12360c20da90e9a7cf7f4010aac1
raketos2013 <daniil123498@mail.ru>
2025-05-29 19:22:05 +0300

    add unitOfWork

---
+++ JobAggregator/JobAggregator.Core/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace JobAggregator.Core.Interfaces.Repositories
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository UserRepository { get; }
+        IRoleRepository RoleRepository { get; }
+        Task<int> SaveAsync();
+    }
+}
